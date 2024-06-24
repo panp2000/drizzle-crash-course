138263dc9fa24136fa3c571f64d9af645700f6f0
panp2000 <tsuyoshi.suenaga@gmail.com>
2024-06-24 22:52:51 +0900

    SQL Style Data Select

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,32 @@
 import "dotenv/config";
 import { db } from "./drizzle/db";
 import { UserPreferencesTable, UserTable } from "./drizzle/schema";
-import { asc, desc, sql } from "drizzle-orm";
+import { count, eq, gt } from "drizzle-orm";
 
 async function main() {
-  // await db.insert(UserPreferencesTable).values({
-  //   emailUpdates: true,
-  //   userId: "6a374f77-c085-4683-ad49-08abb589e3f3",
-  // });
-  const users = await db.query.UserTable.findMany({
-    // columns: { name: true, email: true, id: true },
-    columns: { age: true, id: true },
-    // extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") },
-    // limit: 1,
-    // offset: 1,
-    // with: { preferences: true },
-    // with: { preferences: {
-    //   columns: {
-    //     emailUpdates: true,
-    //   }
-    // }},
-    with: {
-      posts: {
-        with: { postCategories: true },
-      }
-    },
-    // orderBy: asc(UserTable.age)
-    // orderBy: desc(UserTable.age)
-    // orderBy: (table, {asc}) => asc(table.age),
-    // where: (table, funcs) => funcs.eq(table.age, 25),
-    // where: (table, funcs) => funcs.between(table.age, 20, 25),
-    where: (table, funcs) => funcs.gt(table.age, 20),
-  });
+  const users = await db
+    .select({
+      id: UserTable.id,
+      age: UserTable.age,
+      emailUpdates: UserPreferencesTable.emailUpdates,
+    })
+    .from(UserTable)
+    // .where(eq(UserTable.age, 29))
+    .leftJoin(
+      UserPreferencesTable,
+      eq(UserPreferencesTable.userId, UserTable.id)
+    );
+  const users2 = await db
+    .select({
+      name: UserTable.name,
+      count: count(UserTable.name)
+    })
+    .from(UserTable)
+    .groupBy(UserTable.name)
+    .having(columns => gt(columns.count, 1));
 
   console.log(users);
+  console.log(users2);
 }
 
 main();
