80dec36247ce320abf8f0a83a87c37c673dd7006
panp2000 <tsuyoshi.suenaga@gmail.com>
2024-06-24 20:56:32 +0900

    Query Style Data Select

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import {
   boolean,
   index,
@@ -55,3 +56,57 @@ export const PostCategoryTable = pgTable("postCategory", {
     pk: primaryKey({ columns: [table.postId, table.categoryId] }),
   };
 });
+
+// RELATIONS
+
+export const UserTableRelations = relations(UserTable, ({ one, many }) => {
+  return {
+    preferences: one(UserPreferencesTable),
+    posts: many(PostTable),
+  };
+});
+
+export const UserPreferencesTableRelations = relations(
+  UserPreferencesTable,
+  ({ one }) => {
+    return {
+      user: one(UserTable, {
+        fields: [UserPreferencesTable.userId],
+        references: [UserTable.id]
+      })
+    };
+  });
+
+export const PostTableRelations = relations(
+  PostTable,
+  ({ one, many }) => {
+    return {
+      author: one(UserTable, {
+        fields: [PostTable.autorId],
+        references: [UserTable.id]
+      }),
+      postCategories: many(PostCategoryTable),
+    };
+  });
+
+export const CategoryTableRelations = relations(CategoryTable, ({ many }) => {
+  return {
+    postCategories: many(PostCategoryTable),
+  };
+});
+
+export const PostCategoryTableRelations = relations(
+  PostCategoryTable,
+  ({ one }) => {
+    return {
+      post: one(PostTable, {
+        fields: [PostCategoryTable.postId],
+        references: [PostTable.id],
+      }),
+      category: one(CategoryTable, {
+        fields: [PostCategoryTable.categoryId],
+        references: [CategoryTable.id],
+      })
+    };
+  }
+);
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
 import "dotenv/config";
 import { db } from "./drizzle/db";
-import { UserTable } from "./drizzle/schema";
+import { UserPreferencesTable, UserTable } from "./drizzle/schema";
+import { asc, desc, sql } from "drizzle-orm";
 
 async function main() {
-  await db.delete(UserTable);
-  const user = await db.insert(UserTable).values([
-    {
-      name: "Kyle",
-      age: 29,
-      email: "test@test.com"
-    }, { name: "Sally", age: 25, email: "test@test2.com" }
-  ]).returning({
-    id: UserTable.id,
-    userName: UserTable.name,
-  }).onConflictDoUpdate({
-    target: UserTable.email,
-    set: { name: "Updated Name" },
+  // await db.insert(UserPreferencesTable).values({
+  //   emailUpdates: true,
+  //   userId: "6a374f77-c085-4683-ad49-08abb589e3f3",
+  // });
+  const users = await db.query.UserTable.findMany({
+    // columns: { name: true, email: true, id: true },
+    columns: { age: true, id: true },
+    // extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") },
+    // limit: 1,
+    // offset: 1,
+    // with: { preferences: true },
+    // with: { preferences: {
+    //   columns: {
+    //     emailUpdates: true,
+    //   }
+    // }},
+    with: {
+      posts: {
+        with: { postCategories: true },
+      }
+    },
+    // orderBy: asc(UserTable.age)
+    // orderBy: desc(UserTable.age)
+    // orderBy: (table, {asc}) => asc(table.age),
+    // where: (table, funcs) => funcs.eq(table.age, 25),
+    // where: (table, funcs) => funcs.between(table.age, 20, 25),
+    where: (table, funcs) => funcs.gt(table.age, 20),
   });
 
-  console.log(user);
+  console.log(users);
 }
 
 main();
