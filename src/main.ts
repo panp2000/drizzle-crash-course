a33b17a65b88fddb8b6c7e71e7ef2a914aa443cc
panp2000 <tsuyoshi.suenaga@gmail.com>
2024-06-24 20:29:21 +0900

    Inserting Data

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,21 @@ import { UserTable } from "./drizzle/schema";
 
 async function main() {
   await db.delete(UserTable);
+  const user = await db.insert(UserTable).values([
+    {
+      name: "Kyle",
+      age: 29,
+      email: "test@test.com"
+    }, { name: "Sally", age: 25, email: "test@test2.com" }
+  ]).returning({
+    id: UserTable.id,
+    userName: UserTable.name,
+  }).onConflictDoUpdate({
+    target: UserTable.email,
+    set: { name: "Updated Name" },
+  });
+
+  console.log(user);
 }
 
 main();
