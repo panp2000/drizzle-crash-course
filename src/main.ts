2785dd175ef62dd015030b77e5ddac08cddb8577
panp2000 <tsuyoshi.suenaga@gmail.com>
2024-06-24 20:02:08 +0900

    connect database

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
+import "dotenv/config";
+import { db } from "./drizzle/db";
+import { UserTable } from "./drizzle/schema";
+
 async function main() {
-  console.log("Here");
+  await db.insert(UserTable).values({
+    name: "Kyle"
+  });
+  const user = await db.query.UserTable.findFirst();
+  console.log(user);
 }
 
 main();
