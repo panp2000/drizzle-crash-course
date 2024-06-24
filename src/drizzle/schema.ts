cd159cfb07147ca98f579eacbdc0c972f1afa505
panp2000 <tsuyoshi.suenaga@gmail.com>
2024-06-24 19:52:01 +0900

    Delete unnecessary files

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -2,6 +2,5 @@ import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const UserTable = pgTable("user", {
   id: uuid("id").primaryKey().defaultRandom(),
-  // public_id: serial("id2").primaryKey()
   name: varchar("name", { length: 255 }).notNull(),
 });
