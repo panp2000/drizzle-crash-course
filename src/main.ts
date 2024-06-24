e8ca40694416f433cb496e71f735c845ffa6f468
panp2000 <tsuyoshi.suenaga@gmail.com>
2024-06-24 22:56:41 +0900

    Updating Data

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,15 @@ import { UserPreferencesTable, UserTable } from "./drizzle/schema";
 import { count, eq, gt } from "drizzle-orm";
 
 async function main() {
+  // const users = await db.select().from(UserTable);
   const users = await db
-    .select({
-      id: UserTable.id,
-      age: UserTable.age,
-      emailUpdates: UserPreferencesTable.emailUpdates,
+    .update(UserTable)
+    .set({
+      age: 29
     })
-    .from(UserTable)
-    // .where(eq(UserTable.age, 29))
-    .leftJoin(
-      UserPreferencesTable,
-      eq(UserPreferencesTable.userId, UserTable.id)
-    );
-  const users2 = await db
-    .select({
-      name: UserTable.name,
-      count: count(UserTable.name)
-    })
-    .from(UserTable)
-    .groupBy(UserTable.name)
-    .having(columns => gt(columns.count, 1));
+    .where(eq(UserTable.age, 30));
 
   console.log(users);
-  console.log(users2);
 }
 
 main();
